c212ce174cc7cee7af080d21c612a3e61a01faad
Anis Marrouchi <anismarrouchi@hotmail.com>
2017-09-19 22:23:44 +0100

    Add relation between facility and programs with migration

---
+++ database/migrations/2017_09_19_210824_add_program_to_facility.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddProgramToFacility extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Add program program relation
+        Schema::table('scolar_programs', function (Blueprint $table) {
+            $table->integer('parent_id')->unsigned();
+
+            $table->foreign('parent_id')->references('id')->on('scolar_programs')->onUpdate('cascade')->onDelete('cascade');
+        });
+        // Add program facility relation
+        Schema::create('facility_scolar_programs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('facility_id')->unsigned();
+            $table->integer('scolar_program_id')->unsigned();
+
+            $table->unique(['facility_id', 'scolar_program_id'], 'facility_scolar_program_unique');
+            $table->foreign('scolar_program_id')->references('id')->on('scolar_programs')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('facility_id')->references('id')->on('facilities')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('scolar_programs', function (Blueprint $table) {
+            $table->dropForeign('scolar_programs_parent_id_foreign');
+            $table->dropColumn('parent_id');
+        });
+        Schema::dropIfExists('facility_scolar_programs');
+    }
+}
