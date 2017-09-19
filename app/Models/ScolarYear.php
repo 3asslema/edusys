0c66ec8b5c7db674b36131162baea9794de6a55a
Anis Marrouchi <anismarrouchi@hotmail.com>
2017-09-19 23:10:05 +0100

    Add properties to scolarYear model

--- app/Models/ScolarYear.php
+++ app/Models/ScolarYear.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class ScolarYear extends Model
 {
-    //
+    protected $table = 'scolar_years';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'scolar_program_id','tuition_fee_id'
+    ];
+
+    /**
+     * Get the scolar program relation.
+     */
+    public function program()
+    {
+        return $this->belongsTo(\App\Models\ScolarProgram::class,'scolar_program_id');
+    }
+
+    /**
+     * Get the scolar program relation.
+     */
+    public function cost()
+    {
+        return $this->belongsTo(\App\Models\TuitionFee::class,'tuition_fee_id');
+    }
 }
