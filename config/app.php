4c19d2760eeba68508cfb65ab0207f0a9f5d6c2f
Anis Marrouchi <anismarrouchi@hotmail.com>
2017-09-29 23:53:50 +0100

    Fix migration key length problem/ Add PDF library

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         Barryvdh\Cors\ServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
+
         /*
          * Application Service Providers...
          */
@@ -178,7 +179,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
@@ -229,6 +230,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
