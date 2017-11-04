f71d83da2d92fd291defca138c392a07da6065a9
Anis Marrouchi <anismarrouchi@hotmail.com>
2017-11-04 14:50:22 +0100

    Fix duplicate duplicate subject_unit_id column in course migration

--- database/migrations/2017_10_22_093737_add_fields_to_courses.php
+++ database/migrations/2017_10_22_093737_add_fields_to_courses.php
@@ -14,14 +14,12 @@ class AddFieldsToCourses extends Migration
     public function up()
     {
         Schema::table('courses', function (Blueprint $table) {
-            $table->integer('subject_unit_id')->unsigned();
             $table->integer('tutor_id')->unsigned();
             $table->integer('student_class_id')->unsigned();
             $table->integer('scolar_year_id')->unsigned();
             $table->integer('academic_year_id')->unsigned();
             $table->integer('academic_year_term_id')->unsigned();
 
-            $table->foreign('unit_subject_id')->references('id')->on('subject_units')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('tutor_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('student_class_id')->references('id')->on('student_classes')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('scolar_year_id')->references('id')->on('scolar_years')->onUpdate('cascade')->onDelete('cascade');
@@ -39,14 +37,12 @@ class AddFieldsToCourses extends Migration
     public function down()
     {
         Schema::table('courses', function (Blueprint $table) {
-            $table->dropForeign('courses_subject_unit_id_foreign');
             $table->dropForeign('courses_tutor_id_foreign');
             $table->dropForeign('courses_student_class_id_foreign');
             $table->dropForeign('courses_scolar_year_id_foreign');
             $table->dropForeign('courses_academic_year_id_foreign');
             $table->dropForeign('courses_academic_year_term_id_foreign');
 
-            $table->dropColumn('subject_unit_id');
             $table->dropColumn('tutor_id');
             $table->dropColumn('student_class_id');
             $table->dropColumn('scolar_year_id');
